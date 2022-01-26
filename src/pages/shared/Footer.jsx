e07f735ae6cedf0b8ff56875dbfca548e8ddbbbb
Babajide Peter Oyafemi <67855455+Babajide777@users.noreply.github.com>
2022-01-26 06:00:30 -0600

    added react icons and upwork icon

--- src/pages/shared/Footer.jsx
+++ src/pages/shared/Footer.jsx
@@ -1,46 +1,41 @@
 import React from "react";
-import { Github, Linkedin, Twitter } from "react-bootstrap-icons";
+import { FilePptFill, Github, Linkedin, Twitter } from "react-bootstrap-icons";
+import { SiUpwork } from "react-icons/si";
 
 const Footer = () => {
   return (
     <>
-      <br />
-      {/* <footer className="fixed-bottom">
-        <div className="container ">
-          <div className="bg-light d-flex justify-content-end align-items-center">
-            Project Explorer <span>&#169;</span> 2021 .<b>EdConnect</b>
-          </div>
-        </div>
-      </footer> */}
-
-      <div className="container fixed-bottom">
+      <div className="container fixed-bottom mt-3">
         <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
           <div className="col-md-4 d-flex align-items-center">
             <a
               href="/"
-              className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1"
+              className="mb-3 me-2 mb-md-0 text-primary text-decoration-none lh-1"
             >
-              <svg className="bi" width="30" height="24">
-                {/* <use xlink:href="#bootstrap" /> */}
-              </svg>
+              <FilePptFill></FilePptFill>
             </a>
             <span className="text-muted">&copy; 2022 Oyafemi Babajide</span>
           </div>
 
           <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
             <li className="ms-3">
-              <a className="text-muted" href="#">
+              <a className="" href="#">
                 <Github></Github>
               </a>
             </li>
             <li className="ms-3">
-              <a className="text-muted" href="#">
+              <a className="" href="#">
                 <Linkedin></Linkedin>
               </a>
             </li>
             <li className="ms-3">
-              <a className="text-muted" href="#">
+              <a className="" href="#">
                 <Twitter></Twitter>
+              </a>
+            </li>
+            <li className="ms-3">
+              <a className="" href="#">
+                <SiUpwork></SiUpwork>
               </a>
             </li>
           </ul>
