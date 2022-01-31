b94281b04e2c834d9bc8b3b4be78a47ad84f839b
Babajide Peter Oyafemi <67855455+Babajide777@users.noreply.github.com>
2022-01-31 05:13:26 -0600

    setup state for form submission.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,18 +1,69 @@
-import React, { useState } from "react";
+import React, { useReducer, useState } from "react";
 import Layout from "./shared/Layout";
 import { Alert, Button, Col, Container, Form, Row } from "react-bootstrap";
 import { Facebook, Google } from "react-bootstrap-icons";
 
-const BuildForm = ({ err }) => {
-  const [showAlert, setShowAlert] = useState(false);
-  // err.length > 0 ? (showAlert = true) : (showAlert = false);
+const initialState = {
+  showAlert: false,
+  errMsg: "",
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "NO_EMAIL_VALUE":
+      return {
+        showAlert: true,
+        errMsg: "Email can not be empty",
+      };
+
+    case "NO_PASSWORD_VALUE":
+      return {
+        showAlert: true,
+        errMsg: "Password can not be empty",
+      };
+
+    case "REFRESH":
+      return {
+        ...state,
+        showAlert: false,
+      };
+
+    default:
+      throw new Error("no matching action type");
+  }
+};
+
+const BuildForm = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch({ type: "REFRESH" });
+    if (email === "") {
+      dispatch({ type: "NO_EMAIL_VALUE" });
+    }
+    if (password === "") {
+      dispatch({ type: "NO_PASSWORD_VALUE" });
+    }
+  };
 
   return (
     <div className="mx-auto w-50 p-3 mw-70">
       <h1 className="text-primary mb-4">Login</h1>
-      <Form id="loginForm" method="post" action="/login" className="mb-3">
-        <Alert className="alert alert-danger" variant="danger" show={showAlert}>
-          {err}
+      <Form
+        id="loginForm"
+        method="post"
+        className="mb-3"
+        onSubmit={handleSubmit}
+      >
+        <Alert
+          className="alert alert-danger"
+          variant="danger"
+          show={state.showAlert}
+        >
+          {state.errMsg}
         </Alert>
         <Form.Group controlId="formBasicEmail" className="mb-2">
           <Form.Label className="fw-bold text-primary">
@@ -22,6 +73,8 @@ const BuildForm = ({ err }) => {
             type="email"
             placeholder="Enter your email"
             name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
         </Form.Group>
         <Form.Group controlId="formBasicPassword" className="mb-3">
@@ -30,6 +83,8 @@ const BuildForm = ({ err }) => {
             type="password"
             placeholder="Enter your password"
             name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
         </Form.Group>
         <Button variant="primary" type="submit">
@@ -67,7 +122,7 @@ const BuildForm = ({ err }) => {
 const Login = (props) => {
   return (
     <Layout us={props.us}>
-      <BuildForm {...props} />
+      <BuildForm />
     </Layout>
   );
 };
