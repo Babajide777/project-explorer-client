262d1944fd2162cc177020b665840f6a2483bcfe
Babajide Peter Oyafemi <67855455+Babajide777@users.noreply.github.com>
2022-02-10 12:33:49 -0600

    signup reducer done

--- src/components/MainSignUp.jsx
+++ src/components/MainSignUp.jsx
@@ -1,26 +1,48 @@
-import React from "react";
+import React, { useEffect, useReducer, useState } from "react";
 import { Alert, Button, Col, Container, Form, Row } from "react-bootstrap";
 import { Facebook, Google } from "react-bootstrap-icons";
+import { reducer } from "../reducers/signUpReducer";
+
+const initialState = {
+  showAlert: false,
+  errMsg: [],
+};
 
 const MainSignUp = () => {
+  const [program, setProgram] = useState([]);
+  const [graduationYear, setGraduationYear] = useState([]);
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/home/programs")
+      .then((res) => res.json())
+      .then((res) => setProgram(res.data))
+      .catch((err) => console.log(err));
+
+    fetch("http://localhost:4000/home/graduationyears")
+      .then((res) => res.json())
+      .then((res) => setGraduationYear(res.data))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div className="mx-auto w-50 p-3 mw-70">
       <h1 className="text-primary">Signup</h1>
       <Form id="signupForm" className="mb-3">
-        {/* <Alert
+        <Alert
           className="alert alert-danger"
           variant="danger"
-          show={showAlert}
+          show={state.showAlert}
         >
-          {err.map((text) => {
-              return (
-                <>
-                  {text}
-                  <br />
-                </>
-              );
-            })}
-        </Alert> */}
+          {state.errMsg.map((text) => {
+            return (
+              <>
+                {text}
+                <br />
+              </>
+            );
+          })}
+        </Alert>
         <Row className="mb-3">
           <Form.Group as={Col} md={6}>
             <Form.Label className="fw-bold text-primary">First Name</Form.Label>
@@ -47,31 +69,31 @@ const MainSignUp = () => {
         <Row className="mb-3">
           <Form.Group as={Col} md={6}>
             <Form.Label className="fw-bold text-primary">Program</Form.Label>
-            <Form.Control as="select" name="program">
+            <Form.Select name="program">
               <option>Select Option</option>
-              {/* {program.map((prog) => (
-                  <option key={prog}>{prog}</option>
-                ))} */}
-            </Form.Control>
+              {program.map((prog) => (
+                <option key={prog}>{prog}</option>
+              ))}
+            </Form.Select>
           </Form.Group>
 
           <Form.Group as={Col}>
             <Form.Label className="fw-bold text-primary">
               Matric Number
             </Form.Label>
-            <Form.Control name="matricNumber" />
+            <Form.Control type="text" name="matricNumber" />
           </Form.Group>
 
           <Form.Group as={Col}>
             <Form.Label className="fw-bold text-primary">
               Graduation Year
             </Form.Label>
-            <Form.Control as="select" name="graduationYear">
+            <Form.Select as="select" name="graduationYear">
               <option>Select Option</option>
-              {/* {graduationYear.map((year) => (
-                  <option key={year}>{year}</option>
-                ))} */}
-            </Form.Control>
+              {graduationYear.map((year) => (
+                <option key={year}>{year}</option>
+              ))}
+            </Form.Select>
           </Form.Group>
         </Row>
 
---
+++ src/reducers/signUpReducer.js
@@ -0,0 +1,9 @@
+export const reducer = (state, action) => {
+  switch (action.type) {
+    case "NO_VALUE":
+      break;
+
+    default:
+      throw new Error("no matching action type");
+  }
+};
