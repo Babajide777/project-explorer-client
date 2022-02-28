8ddafac044e2769651b394d2ecf288c4122b96a0
Babajide Peter Oyafemi <67855455+Babajide777@users.noreply.github.com>
2022-02-28 08:12:29 -0600

    setup form state values and handleSubmit

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,38 +1,71 @@
 import React from "react";
 import Layout from "./shared/Layout";
 import { Alert, Button, Form } from "react-bootstrap";
+import { useState } from "react";
 
 const BuildForm = () => {
-  // let showAlert = false;
-  // err.length > 0 ? (showAlert = true) : (showAlert = false);
+  const [showAlert, setShowAlert] = useState(false);
+  const [errMsg, setErrMsg] = useState([]);
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (email === "") {
+      setShowAlert(true);
+      setErrMsg(["Email field can not be empty"]);
+    } else {
+      setShowAlert(false);
+      setErrMsg([]);
+      fetch("http://localhost:4000/user/login", {
+        method: "POST",
+        body: JSON.stringify({ email }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((res) => res.json())
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((err) => console.log(err));
+    }
+  };
   return (
     <>
       <div className="mx-auto w-50 p-3 mw-70 mb-5">
         <h2 className="text-primary fw-bold mb-4">Forgot Password?</h2>
-        <Form id="loginForm" method="post" action="/forgotpassword">
-          {/* <Alert
+        <Form id="loginForm" onSubmit={handleSubmit}>
+          <Alert
             className="alert alert-danger"
             variant="danger"
             show={showAlert}
           >
-            {err}
-          </Alert> */}
-
+            {errMsg.map((text) => {
+              return (
+                <>
+                  <small key={text}>{text}</small>
+                  <br />
+                </>
+              );
+            })}
+          </Alert>
           <Alert className="alert alert-primary" variant="primary">
             Enter your email address to begin the password reset process
           </Alert>
 
           <Form.Group controlId="formBasicEmail" className="mb-4">
-            <Form.Control type="email" placeholder="Enter email" name="email" />
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
           </Form.Group>
           <Button variant="primary" type="submit">
             Submit
           </Button>
-          <a
-            href="/login"
-            className="link-primary"
-            style={{ marginLeft: 15 + "px" }}
-          >
+          <a href="/login" className="link-primary ms-3">
             Login
           </a>
         </Form>
