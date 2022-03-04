87e83d1e786a4b33eaf4ed7b6108f47817793ccd
Babajide Peter Oyafemi <67855455+Babajide777@users.noreply.github.com>
2022-03-04 13:46:53 -0600

    added default reducer cases

--- src/pages/CreateProject.jsx
+++ src/pages/CreateProject.jsx
@@ -20,25 +20,70 @@ const CreateProjectForm = ({ err }) => {
   const [tags, setTags] = useState("");
   const [state, dispatch] = useReducer(reducer, initialState);
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch({ type: "REFRESH" });
+    if (name === "") {
+      dispatch({ type: "NO_NAME_VALUE" });
+    }
+    if (abstract === "") {
+      dispatch({ type: "NO_ABSTRACT_VALUE" });
+    }
+    if (author === "") {
+      dispatch({ type: "NO_AUTHOR_VALUE" });
+    }
+    if (tags === "") {
+      dispatch({ type: "NO_TAG_VALUE" });
+    }
+    console.log(author);
+    console.log(tags);
+
+    if (!(name === "" || abstract === "" || author === "" || tags === "")) {
+      // fetch("http://localhost:4000/user/resetpassword", {
+      //   method: "POST",
+      //   body: JSON.stringify({ name, abstract }),
+      //   headers: {
+      //     "Content-Type": "application/json",
+      //   },
+      // })
+      //   .then((res) => res.json())
+      //   .then((res) => {
+      //     console.log(res);
+      //     if (res.success) {
+      //       dispatch({ type: "SUCCESS", payload: res.message });
+      //       setTimeout(() => {
+      //         navigate("/");
+      //       }, 5000);
+      //     }
+      //     dispatch({
+      //       type: "USER_UNAUTHETICATION",
+      //       payload: res.message,
+      //     });
+      //   })
+      //   .catch((err) => console.log(err));
+    }
+  };
+
   return (
     <>
       <div className="mx-auto w-50 p-3 mw-70">
         <h2 className="text-primary mb-4">Submit Project</h2>
-        <Form>
-          {/* <Alert
-            className="alert alert-danger"
-            variant="danger"
-            show={showAlert}
+        <Form onSubmit={handleSubmit}>
+          <Alert
+            className={state.alertClass}
+            variant={state.alertVariant}
+            show={state.showAlert}
           >
-            {err.map((text) => {
+            {state.errMsg.map((text) => {
               return (
                 <>
-                  {text}
+                  <small key={text}>{text}</small>
                   <br />
                 </>
               );
             })}
-          </Alert> */}
+          </Alert>
+
           <Form.Group>
             <Form.Label className="fw-bold text-primary">
               Project Name
--- src/reducers/createProjectReducer.js
+++ src/reducers/createProjectReducer.js
@@ -1,1 +1,50 @@
-export const reducer = (state, action) => {};
+export const reducer = (state, action) => {
+  switch (action.type) {
+    case "REFRESH":
+      return {
+        showAlert: false,
+        errMsg: [],
+        alertVariant: "danger",
+        alertClass: "alert alert-danger",
+      };
+
+    case "NO_NAME_VALUE":
+      return {
+        ...state,
+        showAlert: true,
+        alertVariant: "danger",
+        alertClass: "alert alert-danger",
+        errMsg: [...state.errMsg, "Please fill in your name"],
+      };
+
+    case "NO_ABSTRACT_VALUE":
+      return {
+        ...state,
+        showAlert: true,
+        alertVariant: "danger",
+        alertClass: "alert alert-danger",
+        errMsg: [...state.errMsg, "Please fill in the project's abstract"],
+      };
+
+    case "NO_AUTHOR_VALUE":
+      return {
+        ...state,
+        showAlert: true,
+        alertVariant: "danger",
+        alertClass: "alert alert-danger",
+        errMsg: [...state.errMsg, "Please fill in the project's author"],
+      };
+
+    case "NO_TAG_VALUE":
+      return {
+        ...state,
+        showAlert: true,
+        alertVariant: "danger",
+        alertClass: "alert alert-danger",
+        errMsg: [...state.errMsg, "Please fill in relevant project tags"],
+      };
+
+    default:
+      throw new Error("no matching action type");
+  }
+};
