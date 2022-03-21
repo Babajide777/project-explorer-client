8866b7c40d93b0ea1f2d136db1fbae3b885183aa
Babajide Peter Oyafemi <67855455+Babajide777@users.noreply.github.com>
2022-03-21 04:45:55 -0500

    basic continue signup layout

--- src/components/Continue.jsx
+++ src/components/Continue.jsx
@@ -4,10 +4,22 @@ import { Alert, Button, Form } from "react-bootstrap";
 import { setToken } from "../auth";
 
 const Continue = () => {
+  const [programs, setPrograms] = useState([]);
+  const [graduationYears, setGraduationYears] = useState([]);
   const [user, setUser] = useState({});
   const { id } = useParams();
   let navigate = useNavigate();
   useEffect(() => {
+    fetch("http://localhost:4000/home/programs")
+      .then((res) => res.json())
+      .then((res) => setPrograms(res.data))
+      .catch((err) => console.log(err));
+
+    fetch("http://localhost:4000/home/graduationyears")
+      .then((res) => res.json())
+      .then((res) => setGraduationYears(res.data))
+      .catch((err) => console.log(err));
+
     fetch("http://localhost:4000/user/continuesignup", {
       method: "POST",
       headers: {
@@ -32,7 +44,41 @@ const Continue = () => {
 
   return (
     <div className="mx-auto w-50 p-3 mw-60">
-      <h2>Continue your signup</h2>
+      <h1 className="text-primary mb-4">Continue your signup process</h1>
+      <Form>
+        <Form.Group>
+          <Form.Label className="fw-bold text-primary">
+            Matric Number
+          </Form.Label>
+          <Form.Control name="matricNumber" />
+        </Form.Group>
+
+        <Form.Group>
+          <Form.Label className="fw-bold text-primary">Program</Form.Label>
+          <Form.Select name="program">
+            <option>Select Option</option>
+            {programs.map((prog) => (
+              <option key={prog} value={prog}>
+                {prog}
+              </option>
+            ))}
+          </Form.Select>
+        </Form.Group>
+
+        <Form.Group>
+          <Form.Label className="fw-bold text-primary">
+            Graduation Year
+          </Form.Label>
+          <Form.Select as="select" name="graduationYear">
+            <option>Select Option</option>
+            {graduationYears.map((year) => (
+              <option key={year} value={year}>
+                {year}
+              </option>
+            ))}
+          </Form.Select>
+        </Form.Group>
+      </Form>
     </div>
   );
 };
